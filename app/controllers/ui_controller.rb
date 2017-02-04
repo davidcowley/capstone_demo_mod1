10da02a1595dcca0aad70a9ace686d42a64e08da
davidcowley <davidcowley@4fast.net>
2017-02-04 08:47:43 -0800

    added redirect

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -1,2 +1,8 @@
 class UiController < ApplicationController
+
+#  def index
+    #redirect_to "/client/index.html", :status=>:moved_permanently
+#    redirect_to "https://davidcowley.github.io/dwc-spa-demo/public/client/index.html", :status=>:moved_permanently
+# end
+
 end
