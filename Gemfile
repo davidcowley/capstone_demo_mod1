0b6f1da8dffe95ec2f1491ee5cd5d97f3ef4b6e8
davidcowley <davidcowley@4fast.net>
2017-02-11 18:23:15 -0800

    testing module

--- Gemfile
+++ Gemfile
@@ -26,6 +26,13 @@ group :development, :test do
   gem 'httparty', '~>0.14', '>=0.14.0'
 
   gem 'rspec-rails', '~>3.5', '>=3.5.2'
+  gem 'mongoid-rspec', '~> 3.0', '>=3.0.0'
+  gem 'capybara', '~> 2.10', '>=2.10.1'
+  gem 'poltergeist', '~> 1.11', '>=1.11.0'
+  gem 'selenium-webdriver', '~> 2.53', '>=2.53.4'
+  gem 'chromedriver-helper', '~>1.0', '>=1.0.0'
+  gem 'launchy', '~>2.4', '>=2.4.3'
+  gem 'simplecov', '~>0', '>=0.12', :require=>false
 end
 
 group :production do
@@ -35,6 +42,9 @@ end
 gem 'puma', '~>3.6', '>=3.6.0', :platforms=>:ruby
 gem 'pg', '~>0.19', '>=0.19.0'
 gem 'mongoid', '~>5.1', '>=5.1.5'
+gem 'database_cleaner', '~>1.5', '>=1.5.3'
+gem 'factory_girl_rails', '~>4.7', '>=4.7.0'
+gem 'faker', '~>1.6', '>=1.6.6'
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-bootstrap', '~>3.3', '>= 3.3.7'
--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "ApiDevelopments", type: :request do
- 
+  include_context "db_cleanup_each"
   def parsed_body
     JSON.parse(response.body)
   end
 
   describe "RDBMS-backed" do
-  	before(:each){City.delete_all}
-   	after(:each){City.delete_all}
- 	
   	it "create RDBMS-backed model" do
       city_object=City.create(:name=>"Montague")
       expect(City.find(city_object.id).name).to eq("Montague")
   	end
+    
   	it "expose RDBMS-backed API resource" do
      city_object=City.create(:name=>"Montague")
      expect(cities_path).to eq("/api/cities")
@@ -25,9 +23,6 @@ RSpec.describe "ApiDevelopments", type: :request do
 
 
   describe "MongoDB-backed" do
- 	  before(:each){State.delete_all}
-   	after(:each){State.delete_all}
-
    	it "create MongoDB-backed model" do
       state_object=State.create(:name=>"California")
       expect(State.find(state_object.id).name).to eq("California")
